ab33fd647717c2d584884231db2bae840bdad5b1
zakram2001 <87619768+zakram2001@users.noreply.github.com>
2021-11-20 20:51:55 -0500

    lab 6

---
+++ app/src/main/java/com/androidatc/android_lab_lesson_06/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.androidatc.android_lab_lesson_06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun display(view: View){
+        info.text="Android Application Development, Android Security Essentials and Monetize Android Applications"
+    }
+}
